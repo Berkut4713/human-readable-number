bd80697db3b2ca5845c8b80be8d22c90d41960b3
Berkut4713 <berkut_bm@mail.ru>
2022-06-29 22:30:24 +0300

    feat: add units

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function toReadable (number) {
-  
+    let result = '';
+
+    return units(number);
+}
+function units(params) {
+    switch (params) {
+        case 1:params = 'one';break;
+        case 2:params = 'two';break;
+        case 3:params = 'three';break;
+        case 4:params = 'four';break;
+        case 5:params = 'five';break;
+        case 6:params = 'six';break;
+        case 7:params = 'seven';break;
+        case 8:params = 'eight';break;
+        case 9:params = 'nine';break;
+        default:params = "Нет таких значений";
+      }
+    return params;  
 }
